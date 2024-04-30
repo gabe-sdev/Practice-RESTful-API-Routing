afa843510e79e0ae2e283c1d66a1d75fea1eb4d8
gabe-sdev <glgonzalez.sdev@gmail.com>
2024-04-30 13:57:37 -0400

    Fixed PEP8 warnings on main.py

--- main.py
+++ main.py
@@ -5,24 +5,15 @@ from sqlalchemy import Integer, String, Boolean
 import random
 # from flask.ext.restless import APIManager
 
-'''
-Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
+#Initialize Flask app
 app = Flask(__name__)
 
-# CREATE DB
+
+# Create DB
 class Base(DeclarativeBase):
     pass
+
+
 # Connect to Database
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
 db = SQLAlchemy(model_class=Base)
@@ -34,20 +25,19 @@ db.init_app(app)
 # manager.create_api(report-closed, methods=['DELETE'])
 
 
-
 # Cafe TABLE Configuration
 class Cafe(db.Model):
-    id: Mapped[int]=mapped_column(Integer, primary_key=True)
-    name: Mapped[str]=mapped_column(String(250), unique=True, nullable=False)
-    map_url: Mapped[str]=mapped_column(String(500), nullable=False)
-    img_url: Mapped[str]=mapped_column(String(500), nullable=False)
-    location: Mapped[str]=mapped_column(String(250), nullable=False)
-    seats: Mapped[str]=mapped_column(String(250), nullable=False)
-    has_toilet: Mapped[bool]=mapped_column(Boolean, nullable=False)
-    has_wifi: Mapped[bool]=mapped_column(Boolean, nullable=False)
-    has_sockets: Mapped[bool]=mapped_column(Boolean, nullable=False)
-    can_take_calls: Mapped[bool]=mapped_column(Boolean, nullable=False)
-    coffee_price: Mapped[str]=mapped_column(String(250), nullable=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    map_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(500), nullable=False)
+    location: Mapped[str] = mapped_column(String(250), nullable=False)
+    seats: Mapped[str] = mapped_column(String(250), nullable=False)
+    has_toilet: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_wifi: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    has_sockets: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    can_take_calls: Mapped[bool] = mapped_column(Boolean, nullable=False)
+    coffee_price: Mapped[str] = mapped_column(String(250), nullable=True)
 
     def to_dict(self):
         # Method 1.
@@ -63,6 +53,8 @@ class Cafe(db.Model):
         # Method 2. Altenatively use Dictionary Comprehension to do the same thing.
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
+
+#Create DB tables
 with app.app_context():
     db.create_all()
 
@@ -124,6 +116,7 @@ def search_cafes():
     else:
         return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."}), 404
 
+
 # HTTP POST - Create Record
 @app.route("/add", methods=["POST"])
 def add_cafe():
@@ -131,20 +124,20 @@ def add_cafe():
                 name=request.form["name"],
                 map_url=request.form["map_url"],
                 img_url=request.form["img_url"],
-                location= request.form["location"],
-                seats= request.form["seats"],
-                has_toilet= bool(request.form["has_toilet"]),
-                has_wifi= bool(request.form["has_wifi"]),
-                has_sockets= bool(request.form["has_sockets"]),
-                can_take_calls= bool(request.form["can_take_calls"]),
-                coffee_price= request.form["coffee_price"]
+                location=request.form["location"],
+                seats=request.form["seats"],
+                has_toilet=bool(request.form["has_toilet"]),
+                has_wifi=bool(request.form["has_wifi"]),
+                has_sockets=bool(request.form["has_sockets"]),
+                can_take_calls=bool(request.form["can_take_calls"]),
+                coffee_price=request.form["coffee_price"]
                 )
     db.session.add(data)
     db.session.commit()
     return jsonify(response={"success": "Successfully added the new cafe."})
 
-# HTTP PUT/PATCH - Update Record
 
+# HTTP PUT/PATCH - Update Record
 @app.route("/update-price/<int:cafe_id>")
 def update_price(cafe_id):
     u_price = request.args.get("new_price")
@@ -156,20 +149,21 @@ def update_price(cafe_id):
     else:
         return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
 
-# HTTP DELETE - Delete Record
 
+# HTTP DELETE - Delete Record
 @app.route("/report-closed/<int:cafe_id>", methods=["DELETE"])
 def delete_cafe(cafe_id):
     api_key = request.args.get("api-key")
     if api_key == "test":
-        cafee_to_remove = db.session.get(Cafe, cafe_id)
-        db.session.delete(cafee_to_remove)
+        cafe_to_remove = db.session.get(Cafe, cafe_id)
+        db.session.delete(cafe_to_remove)
         db.session.commit()
         return jsonify(response={"success": "Successfully deleted the coffee shop."}), 200
     elif cafe_id > 22:
         return jsonify(error={"Not Found": "Sorry a cafe with that id was not found in the database."}), 404
     else:
         return jsonify(error="Sorry, that's not allowed. Make sure you have the correct api_key"), 403
+
 
 if __name__ == '__main__':
     app.run(debug=True)
